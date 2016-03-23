a04486fd3e9f60c73d90f16233010cf87087add2
Michele Dolfi <dolfim@phys.ethz.ch>
2016-03-23 21:34:20 +0100

    cleanup

--- run.py
+++ run.py
@@ -100,17 +100,6 @@ def index():
     
     return render_template('index.html', form=form, user=session['gitlab_user'])
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     error = None
-#     if request.method == 'POST':
-#         if request.form['username'] != 'admin' or \
-#                 request.form['password'] != 'secret':
-#             error = 'Invalid credentials'
-#         else:
-#             flash('You were successfully logged in')
-#             return redirect(url_for('index'))
-#     return render_template('login.html', error=error)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
