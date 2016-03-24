514e7e47aaa1cac3a4daff4e5edc8f38b92d8022
Michele Dolfi <dolfim@phys.ethz.ch>
2016-03-24 12:39:04 +0100

    add license

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2016  Theoretical Physics, ETH Zurich
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from flask import Flask, flash, redirect, render_template, \
      request, url_for, session
 from flask_oauthlib.client import OAuth, parse_response
@@ -6,6 +20,7 @@ from wtforms.fields.html5 import EmailField
 from wtforms.csrf.session import SessionCSRF
 
 import requests, json
+
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.cfg')
