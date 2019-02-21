fa33378f946369d07994a83f384c1a283ec85d5b
Michele Dolfi <michele.dolfi@gmail.com>
2019-02-21 19:25:40 +0100

    import print_function

--- run.py
+++ run.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function
+
 from flask import Flask, flash, redirect, render_template, \
      request, url_for, session
 from flask_oauthlib.client import OAuth, parse_response
